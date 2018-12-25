415c63e4de63805f2f068bb9a5c3478c8959abc2
khien.pham <phamkhien@hotmail.com>
2018-12-25 22:32:19 +0700

    fix failed tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialConfigurationModule } from './shared/material-configuration/material-configuration.module';
 import { LoadingScreenInterceptor } from './core/interceptors/loading.interceptor';
-import { LoadingScreenComponent } from './shared/loading-screen/loading-screen.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
